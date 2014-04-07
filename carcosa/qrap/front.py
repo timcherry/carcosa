c858da1be585e0fa27e6a3fdfb4d5362d7c58882
Tim Cherry <timcherry21@gmail.com>
2014-04-06 17:37:22 -0700

    pretty dates

--- carcosa/qrap/front.py
+++ carcosa/qrap/front.py
@@ -5,6 +5,8 @@ from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
 from models import Post
 from django.template import RequestContext, loader
+from datetime import datetime
+
 import redis
 
 SSET_KEY = "popular"
@@ -17,8 +19,27 @@ class Front(View):
         posts = []
         for id in self.redis_c.zrange(SSET_KEY, 0, self.MAX_RANGE, desc=True):
             try:
-                posts.append(Post.objects.get(id=id))
-            except DoesNotExist, ValidationError:
+                post = Post.objects.get(id=id)
+                post.pretty_date = self.get_pretty_date(post.sub_date)
+                posts.append(post)
+            except:
                 print "ID MISMATCH. ID:%s NOT FOUND IN MONGODB"%(id)
                 continue
         return render(request, 'index.html', {"posts": posts })
+
+    @classmethod
+    def get_pretty_date(cls, date):
+        diff =  datetime.utcnow() - date
+        if diff.days:
+            mag = diff.days
+            unit = "days" if mag > 1 else "days"
+        elif diff.seconds >= 60*60:
+            mag = diff.seconds/60/60
+            unit = "hours" if mag > 1 else "hour"
+        elif diff.seconds >= 60:
+            mag = diff.seconds/60
+            unit = "minutes" if mag > 1 else "minute"
+        else:
+            mag = diff.seconds
+            unit = "seconds" if mag > 1 else "second"
+        return "Submitted %s %s ago."%(mag,unit)
