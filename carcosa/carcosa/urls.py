a706c4a1fbed0981b2d94e7319dced8a8b9a2fb4
Tim Cherry <timcherry21@gmail.com>
2014-04-16 21:21:16 -0700

    rename

--- carcosa/carcosa/urls.py
+++ carcosa/carcosa/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^post', PostAPI.as_view(), name='post-view'),
     url(r'^reveal', Reveal.as_view(), name='reveal-view'),
-    url(r'^c/(?P<company>\w+)', Company.as_view(), name='company-view'),
+    url(r'^company/(?P<company>\w+)', Company.as_view(), name='company-view'),
     url(r'^comment/(?P<comment>\w+)', Comment.as_view(), name='comment-view'),
     url(r'', Front.as_view(), name='post-view')
 )
